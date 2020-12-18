d7adb160e31f617d16391f3d8e2c4b7b6136931f
H1ghfield <74437003+H1ghfield@users.noreply.github.com>
2020-12-18 16:29:01 +0100

    Add files via upload

---
+++ minitest.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+clear
+
+echo "a) bestand aanmaken"
+echo "b) bestand checken"
+echo "c) bestand wissen"
+
+read -p "Geef de letter in van u keuze> " keuze
+
+teller=0
+
+if [ $keuze = "a" ]; then
+	clear
+	read -p "Naam> " naamfile
+	read -p "Getal> " getalfile
+	while [ $teller -lt $getalfile ]
+	do
+		touch $naamfile-$teller.txt
+		teller=$((teller+1))	
+	done
+	ls
+	echo "$teller Bestand(en) aangemaakt"
+fi
+
+if [ $keuze = "b" ]; then
+	clear
+	read -p "In welke directory moeten bestanden gecheckt worden?> " checkdir
+	ls $checkdir
+	read -p "Bestandsnaam> " checkfile
+	cd $checkdir
+	if [ -e $checkfile ]; then
+		echo "Bestand bestaat"
+	else	
+		echo "Bestand bestaat niet"
+	fi
+fi
+	
+if [ $keuze = "c" ]; then
+	clear
+	read -p "In welke directory moet een bestand verwijderd worden?> " removedir
+	ls $removedir	
+	read -p "Bestandsnaam> " removefile
+	cd $removedir
+	rm $removefile
+	echo "Bestand gewist"
+fi
+	 
+
